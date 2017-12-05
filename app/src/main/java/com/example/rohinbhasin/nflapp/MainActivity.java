b47eb02964ad9de00255c063c84a8b392e66362a
rohinb2 <bhasin.rohin@gmail.com>
2017-12-04 20:31:15 -0600

    Added Menu Bar instead of default ActionBar.

--- app/src/main/java/com/example/rohinbhasin/nflapp/MainActivity.java
+++ app/src/main/java/com/example/rohinbhasin/nflapp/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.widget.TextView;
+import android.support.v7.widget.Toolbar;
 
 import com.example.rohinbhasin.nflapp.JsonClasses.Score;
 import com.google.firebase.auth.FirebaseAuth;
@@ -25,13 +26,19 @@ public class MainActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+        myToolbar.inflateMenu(R.menu.menu_res);
 
         nflScoresListView = (RecyclerView) findViewById(R.id.score_viewer);
         nflScoresListView.setLayoutManager(new
                 LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
 
+
         new SportsFeedsAsyncTask().execute(0);
     }
+
+    
 
     /**
      * Asynchronous task for accessing all of the Game data from the SportsFeeds API.
